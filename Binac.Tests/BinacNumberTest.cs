6402a8c49cd82b021faf9e76e369e7e4c0b2539f
Andrii Kurdiumov <kant2002@gmail.com>
2024-12-24 22:45:08 +0500

    Add tests for addition and substraction

--- Binac.Tests/BinacNumberTest.cs
+++ Binac.Tests/BinacNumberTest.cs
@@ -3,9 +3,17 @@
     [TestClass]
     public sealed class BinacNumberTest
     {
-        [Ignore]
         [DataTestMethod]
-        [DataRow(30_00_00_00, 0.5)]
+        [DataRow(0x20_00_00_00, 0.5)]
+        [DataRow(0x60_00_00_00, -0.5)] // This is not complimentary number. Or it is?
+        [DataRow(0x10_00_00_00, 0.25)]
+        [DataRow(0x50_00_00_00, -0.25)]
+        [DataRow(0x08_00_00_00, 0.125)]
+        [DataRow(0x48_00_00_00, -0.125)]
+        [DataRow(0x30_00_00_00, 0.75)]
+        [DataRow(0x70_00_00_00, -0.75)]
+        [DataRow(0x00_00_00_00, 0.0)]
+        [DataRow(0x40_00_00_00, -0.0)]
         public void ConversionToDouble(int value, double expected)
         {
             var num = new BinacNumber(value);
@@ -13,6 +21,33 @@
             var result = (double)num;
 
             Assert.AreEqual(expected, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(0x20_00_00_00, 0x10_00_00_00, 0.75)]
+        [DataRow(0x10_00_00_00, 0x60_00_00_00, -0.25)]
+        [DataRow(0x10_00_00_00, 0x50_00_00_00, 0.0)]
+        public void Addition(int term1, int term2, double expected)
+        {
+            var first = new BinacNumber(term1);
+            var second = new BinacNumber(term2);
+
+            var result = first + second;
+            
+            Assert.AreEqual(expected, (double)result);
+        }
+        [DataTestMethod]
+        [DataRow(0x20_00_00_00, 0x10_00_00_00, 0.25)]
+        [DataRow(0x10_00_00_00, 0x20_00_00_00, -0.25)]
+        [DataRow(0x10_00_00_00, 0x10_00_00_00, 0.0)]
+        public void Substract(int term1, int term2, double expected)
+        {
+            var first = new BinacNumber(term1);
+            var second = new BinacNumber(term2);
+
+            var result = first - second;
+            
+            Assert.AreEqual(expected, (double)result);
         }
     }
 }
--- Binac/BinacNumber.cs
+++ Binac/BinacNumber.cs
@@ -11,23 +11,38 @@ public struct BinacNumber
 
     public static explicit operator double (BinacNumber number)
     {
+        if (number.value == 0) return 0.0;
+        if (number.value == 0x40_00_00_00) return -0.0;
+
         var єзнак = HasSign(number);
-        var мантіса = (0x3FF0UL << 49) + ((ulong)NormalizeCore(number.value) << (53 - 30));
-        return (єзнак ? -1 : 1) * (BitConverter.UInt64BitsToDouble(мантіса) - 1.0);
+        int shift = 22; /* 52 - 30;*/
+        var baseValue = AbsCore(number.value);
+        ulong exponent = 0x3FE;
+        while (baseValue != 0 && (baseValue & 0x20_00_00_00) == 0)
+        {
+            baseValue <<= 1;
+            shift++;
+            exponent--;
+        }
+
+        baseValue <<= 1;
+
+        var мантіса = (exponent << 52) + ((ulong)AbsCore(baseValue) << shift);
+        return (єзнак ? -1 : 1) * (BitConverter.UInt64BitsToDouble(мантіса));
     }
 
     public static BinacNumber operator +(BinacNumber first, BinacNumber second)
     {
         BinacNumber result;
-        result.value = first.value + second.value;
+        result.value = CollapseCore(ExpandCore(first.value) + ExpandCore(second.value));
         return Normalize(result);
     }
 
     public static BinacNumber operator -(BinacNumber first, BinacNumber second)
     {
         BinacNumber result;
-        result.value = first.value - second.value;
-        return Normalize(result);
+        result.value = CollapseCore(ExpandCore(first.value) - ExpandCore(second.value));
+        return result;
     }
 
     public static BinacNumber operator *(BinacNumber first, BinacNumber second)
@@ -61,10 +76,32 @@ public struct BinacNumber
 
     public static BinacNumber Normalize(BinacNumber a)
     {
+        return new(a.value & 0x7F_FF_FF_FF);
+    }
+
+    public static BinacNumber Abs(BinacNumber a)
+    {
         return new(a.value & 0x3F_FF_FF_FF);
     }
 
     private static int NormalizeCore(int a)
+    {
+        return a & 0x7F_FF_FF_FF;
+    }
+
+    private static int ExpandCore(int a)
+    {
+        if ((a & 0x40_00_00_00) != 0) return -AbsCore(a);
+        return a;
+    }
+
+    private static int CollapseCore(int a)
+    {
+        if (a < 0) return AbsCore(-a) | 0x40_00_00_00;
+        return a;
+    }
+
+    private static int AbsCore(int a)
     {
         return a & 0x3F_FF_FF_FF;
     }
